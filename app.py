f4dd67744a5b8f097a1d829e20309a6fe9802dbc
Majok-Bol <bolmajok001@gmail.com>
2026-08-13 00:51:36 +0300

    added JWT auth

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,redirect,flash,url_for
+from flask import Flask,render_template,redirect,flash,url_for,make_response
 from flask_wtf import FlaskForm,CSRFProtect
 from wtforms import StringField,SubmitField,EmailField,PasswordField
 from wtforms.validators import Email,EqualTo,Length,InputRequired
@@ -16,13 +16,34 @@ from flask_jwt_extended import(
     set_refresh_cookies,
     create_refresh_token
 )
+from flask_bcrypt import Bcrypt
+
+from datetime import timedelta
 #load env variables
 load_dotenv()
 app=Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']=os.getenv("DATABASE_URL")
 # print("URL: ",url)
 app.config['SECRET_KEY']=os.getenv("CSRF_SECRET_KEY")
-# print("sky: ",sky)
+#configure JWT SECRET KEY
+app.config['JWT_SECRET_KEY']=os.getenv("JWT_SECRET_KEY")
+#where to find token
+#store in the cookie
+app.config['JWT_TOKEN_LOCATION']=["cookies"]
+#name for cookie that will store the access token
+app.config['JWT_ACCESS_COOKIE_NAME']="access_token"
+#sent over HTTPS,Production set it to true
+app.config['JWT_COOKIE_SECURE']=False
+#csrf protection for JWT
+app.config['JWT_COOKIE_CSRF_PROTECT']=False #in production set it to True
+#no JS access
+app.config['JWT_COOKIE_HTTPONLY']=False #set it to True in production
+#prevent cross origin requests
+app.config['JWT_COOKIE_SAMESITE']='Lax' #set to Strict in production to only accept requests from same domain or subdomains
+#token expiration for JWT
+app.config['JWT_ACCESS_TOKEN_EXPIRES']=timedelta(minutes=15)
+#refresh token to prevent redirecting to login again
+app.config['JWT_REFRESH_TOKEN_EXPIRES']=timedelta(days=30)
 #initialize app with database
 db=SQLAlchemy(app)
 #initialize flask_migrate
@@ -33,6 +54,12 @@ csrf=CSRFProtect()
 csrf.init_app(app)
 jwt=JWTManager()
 jwt.init_app(app)
+#initialize bcrypt
+bcrypt=Bcrypt()
+bcrypt.init_app(app)
+#initialize csrf protect globally
+csrf=CSRFProtect()
+csrf.init_app(app)
 @app.route("/",methods=["POST","GET"])
 def home():
     return render_template("home.html")
@@ -58,12 +85,15 @@ def register():
 
         if email_exists:
             print("Email address not available.Please choose another email.")
+        #hash password
+        password_hashed=bcrypt.generate_password_hash(password).decode("utf-8")
 
-        user=User(username=username,email=email,password=password)
+        user=User(username=username,email=email,password=password_hashed)
 
         db.session.add(user)
 
         db.session.commit()
+        print("Account created successfully")
 
         return redirect(url_for('login'))
     
@@ -77,25 +107,79 @@ def login():
         username=form.username.data
         password=form.password.data
         #check username
-        username_exists=User.query.filter_by(username=username)
-        if not username_exists:
+        user=User.query.filter_by(username=username).first()
+        print("Username found: ",user)
+        if not user or not bcrypt.check_password_hash(user.password,password):
             print("Invalid username or password")
             return redirect(url_for('login'))
-        return redirect(url_for('dashboard'))
+        #create access token for user
+        access_token=create_access_token(identity=str(user.id))
+        print("Access token generated: ",access_token)
+        #create refresh token 
+        refresh_token=create_refresh_token(identity=str(user.id))
+        print("Refresh token generated: ",refresh_token)
+        # return redirect(url_for('dashboard'))
+        response=make_response(redirect(url_for("dashboard")))
+        #set cookies
+        #set_access_cookies
+        set_access_cookies(response,access_token)
+        #set refresh cookies
+        set_refresh_cookies(response,refresh_token)
+        return response
     return render_template('login.html',form=form)
-
-        
+#refresh route
+@app.route("/refresh",methods=["POST"])
+@jwt_required(refresh=True)
+def refresh():
+    #get user id
+    user_id=get_jwt_identity()
+    print("User id: ",user_id)
+    access_token=create_access_token(identity=user_id)
+    print("Refresh token: ",access_token)
+    response=make_response(redirect(url_for("home")))
+    set_access_cookies(response,access_token)
+    print("Refresh cookies set: ",set_refresh_cookies)
+    return response   
 @app.route("/dashboard",methods=["POST","GET"])
+@jwt_required()
 def dashboard():
-    return render_template("dashboard.html")
+    #get user id
+    user_id=get_jwt_identity()
+    user=db.session.get(User,int(user_id))
+    print("User id: ",user)
+    if not user:
+        return redirect(url_for("login"))
+    return render_template("dashboard.html",user=user)
 @app.route("/logout")
 def logout():
-    return render_template("logout.html")
+    #redirect to login page after logout
+    response=make_response(redirect(url_for("login")))
+    unset_jwt_cookies(response)
+    print("You have been logged out")
+    return response
+    # return render_template("logout.html")
 @app.route("/uploads")
 def uploads():
     return render_template("uploads.html")
 
-
+#customize JWT error messages
+#invalid token provided in the request
+@jwt.invalid_token_loader
+def invalid_token_loader(reason):
+    print("Reason: ",reason)
+    return redirect(url_for("login"))
+#no token provided in the request
+@jwt.unauthorized_loader
+def missing_token_callback(reason):
+    print("Reason: ",reason)
+    return redirect(url_for("login"))
+#expired token provided
+@jwt.expired_token_loader
+def expired_token_loader(jwt_header,jwt_payload):
+    print("JWT Header: ",jwt_header)
+    print("JWT Payload: ",jwt_payload)
+    return redirect(url_for("login"))
+#
 #register form
 class RegisterForm(FlaskForm):
     username=StringField("Username",validators=[Length(min=4)])
--- migrations/versions/50720340feb6_initial_migrations.py
+++ migrations/versions/50720340feb6_initial_migrations.py
@@ -1,8 +1,8 @@
 """initial migrations
 
-Revision ID: 99c2497e6649
+Revision ID: 50720340feb6
 Revises: 
-Create Date: 2026-08-10 00:01:53.561907
+Create Date: 2026-08-13 00:23:32.500460
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '99c2497e6649'
+revision = '50720340feb6'
 down_revision = None
 branch_labels = None
 depends_on = None
